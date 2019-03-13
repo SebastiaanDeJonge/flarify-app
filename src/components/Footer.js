3294545e8c3011368dccc0e07641f34deba0fa85
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-03-13 22:48:10 +0200

    Fix link to GH repo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
             <div className="widget">
                 This application is meant for non-commercial demonstration purposes only. Real solar flare data provided freely by
                 <a href="https://api.nasa.gov/">NASA</a>.<br />
-                Created by <a href="https://github.com/SebastiaanDeJonge">Sebastiaan de Jonge</a>. View project source code on <a href="https://github.com/SebastiaanDeJonge/spring_flarify">GitHub</a>.
+                Created by <a href="https://github.com/SebastiaanDeJonge">Sebastiaan de Jonge</a>. View project source code on <a href="https://github.com/SebastiaanDeJonge/flarify-app">GitHub</a>.
             </div>
         );
     }
