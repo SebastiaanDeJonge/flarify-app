e63daef6e33396b6331887906a5fd35263ac20c7
Sebastiaan de Jonge <mail@sebastiaandejonge.com>
2019-03-13 23:37:46 +0200

    Fix spacing in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,7 @@ export default class Footer extends Component {
 
         return (
             <div className="widget">
-                This application is meant for non-commercial demonstration purposes only. Real solar flare data provided freely by
-                <a href="https://api.nasa.gov/">NASA</a>.<br />
+                This application is meant for non-commercial demonstration purposes only. Real solar flare data provided freely by <a href="https://api.nasa.gov/">NASA</a>.<br />
                 Created by <a href="https://github.com/SebastiaanDeJonge">Sebastiaan de Jonge</a>. View project source code on <a href="https://github.com/SebastiaanDeJonge/flarify-app">GitHub</a>.
             </div>
         );
